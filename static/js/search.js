b5884f59739978334e41e6f9f6dda48a1df7ecfa
droche <doug.roche@yahoo.com>
2013-04-28 03:23:35 -0400

    fixed the new room results not showing in the playlist

--- static/js/search.js
+++ static/js/search.js
@@ -1,7 +1,7 @@
 // Once the api loads call enable the search box.
 function handleAPILoaded() {
-  $('#search-button').attr('disabled', false);
-  // videoApp.setup();
+	$('#search-button').attr('disabled', false);
+	// videoApp.setup();
 }
 
 
@@ -9,70 +9,70 @@ function handleAPILoaded() {
 
 // Search for a given string.
 function search() {
-  var q = $('#query').val();
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    // maxResults: 10
-    // part: 'snippet(title, description, thumbnails)'
-    fields: 'items(id(videoId), snippet(title, thumbnails))',
-    part: 'snippet'
+	var q = $('#query').val();
+	var request = gapi.client.youtube.search.list({
+		q: q,
+		// maxResults: 10
+		// part: 'snippet(title, description, thumbnails)'
+		fields: 'items(id(videoId), snippet(title, thumbnails))',
+		part: 'snippet'
 });
 
-  request.execute(function(response) {
-    var str = JSON.stringify(response.result);
+	request.execute(function(response) {
+		var str = JSON.stringify(response.result);
 
-    //console.log(response);
+		//console.log(response);
 
 /*
-    var searchIds = [];
-    //loop through results and create a json object from id for the dom
-    $.each(response.items, function(i){
-    	//video id's use this to search stats
-	    //console.log(response.items[i].id.videoId);
-	    searchIds.push(response.items[i].id.videoId);
+		var searchIds = [];
+		//loop through results and create a json object from id for the dom
+		$.each(response.items, function(i){
+			//video id's use this to search stats
+			//console.log(response.items[i].id.videoId);
+			searchIds.push(response.items[i].id.videoId);
 	});
 	console.log("searchids");
 	console.log(searchIds);
 */
 	
-   
-	    
+	 
+			
 /*
-   var contentRequest = gapi.client.youtube.videos.list({
-     id: 'wfpL6_0OBuA',
-     part: 'statistics'
-   });
+	 var contentRequest = gapi.client.youtube.videos.list({
+		 id: 'wfpL6_0OBuA',
+		 part: 'statistics'
+	 });
 */
 
 
 	 var statsObject = [];
 
 //MINE
-    //loop through results and create a json object from id for the dom
-    $.each(response.items, function(i){
-	    var statsObject = [];
+		//loop through results and create a json object from id for the dom
+		$.each(response.items, function(i){
+			var statsObject = [];
 
 		var contentRequest = gapi.client.youtube.videos.list({
 			id: response.items[i].id.videoId,
 			part: 'statistics'
-    	});
+			});
 
-    	contentRequest.execute(function(response){
-	    	var stringify = JSON.stringify(response);  
-	    	console.log("view count below");
-	    	console.log(response.items[0].statistics);     
- 			statsObject.push(response.items[0].statistics); 
+			contentRequest.execute(function(response){
+				var stringify = JSON.stringify(response);  
+				console.log("view count below");
+				console.log(response.items[0].statistics);     
+				statsObject.push(response.items[0].statistics); 
 
-     	});
-     	
-     		console.log("stats object below");
-	console.log(statsObject);
-     	
+			});
+			
+				console.log("stats object below");
+				console.log(statsObject);
+			
 /*
-     	console.log("content request");
-     	console.log(contentRequest);
+			console.log("content request");
+			console.log(contentRequest);
 */
-     		
+				
 /*      	statsObject.push(contentRequest); */
 /*      	console.log(contentRequest);	     */
 	});
@@ -100,33 +100,33 @@ function search() {
 //THIS IS THE STATISTICS SEARCH
 
 /*
-   var contentRequest1 = gapi.client.youtube.videos.list({
-     id: 'wfpL6_0OBuA',
-     part: 'statistics'
-   });
+	 var contentRequest1 = gapi.client.youtube.videos.list({
+		 id: 'wfpL6_0OBuA',
+		 part: 'statistics'
+	 });
 	
-   console.log(contentRequest1);
-   
-  
+	 console.log(contentRequest1);
+	 
+	
 
-   contentRequest.execute(function(response){
+	 contentRequest.execute(function(response){
 	 var stringify = JSON.stringify(response);  
-     console.log("view count below");
-     console.log(response.items[0].statistics);     
-   });	
+		 console.log("view count below");
+		 console.log(response.items[0].statistics);     
+	 });	
 */ 
-   
-	    
-	    
-	    
-	    
-	    
-	    
-	    
-    //clear the search results continer first
-   // $('#searchResults').html('');
-    
-    //mytemplate is the the template script to compile
+	 
+			
+			
+			
+			
+			
+			
+			
+		//clear the search results continer first
+	 // $('#searchResults').html('');
+		
+		//mytemplate is the the template script to compile
 	var source = document.getElementById("myTemplate").innerHTML;
 	var template = Handlebars.compile(source);	
 	//placeholder is the parent div
@@ -142,17 +142,17 @@ function search() {
 	//click
 	$('.video-result-wrapper').click(function(){
 		// var id = $(this).data('id');
-    var videoData = $(this).data();
-    // console.log(videoData);
-    // var title = $(this).data-title;
-    // var thumbnail = $(this).data-thumbnail;
+		var videoData = $(this).data();
+		// console.log(videoData);
+		// var title = $(this).data-title;
+		// var thumbnail = $(this).data-thumbnail;
 
 		// console.log(id);
 		$(this).children('#video-result-wrapper').css({'backgroundColor': 'grey'});
 		// console.log($(this).children('#video-result-wrapper'));
-    // videoApp.updatePlaylist(id);
+		// videoApp.updatePlaylist(id);
 
-    addVideo(videoData);
+		addVideo(videoData);
 		// addVideo(id);
 		
 		//update dom correclty later
@@ -171,63 +171,63 @@ function search() {
 
 
 
-    //$.each(response.items, function(i){
+		//$.each(response.items, function(i){
 
 
 /*
-        var videoResult = $(document.createElement('div')).addClass('video-result-wrapper').attr('id',response.items[i].id.videoId);
-        var videoTitle = $(document.createElement('h2')).append(response.items[i].snippet.title);
-        var videoId = $(document.createElement('p')).append(response.items[i].id.videoId);
-        // var videoThumbnail = $(document.createElement('img')).attr('src', response.items[i].snippet.thumbnails.default.url);
-                var videoThumbnail = $(document.createElement('img')).attr('src', response.items[i].snippet.thumbnails.default.url);
-        var videoThumbnailDiv = $(document.createElement('div')).addClass('thumbnail');
-        videoThumbnailDiv.append(videoThumbnail);
-        
-        // videoResult.append(videoTitle).append(videoId).append(videoThumbnail);
-                videoResult.append(videoThumbnailDiv).append(videoTitle).append(videoId);
+				var videoResult = $(document.createElement('div')).addClass('video-result-wrapper').attr('id',response.items[i].id.videoId);
+				var videoTitle = $(document.createElement('h2')).append(response.items[i].snippet.title);
+				var videoId = $(document.createElement('p')).append(response.items[i].id.videoId);
+				// var videoThumbnail = $(document.createElement('img')).attr('src', response.items[i].snippet.thumbnails.default.url);
+								var videoThumbnail = $(document.createElement('img')).attr('src', response.items[i].snippet.thumbnails.default.url);
+				var videoThumbnailDiv = $(document.createElement('div')).addClass('thumbnail');
+				videoThumbnailDiv.append(videoThumbnail);
+				
+				// videoResult.append(videoTitle).append(videoId).append(videoThumbnail);
+								videoResult.append(videoThumbnailDiv).append(videoTitle).append(videoId);
 
 
-        $('#searchResults').append(videoResult);
+				$('#searchResults').append(videoResult);
 */
 
 
 
 /*
-        $(videoResult).on('click', function(){
-          // var id = $(document.createElement('li')).append($(this).attr('id'));
-          // $('#playlist').append(id);
-          
-          //MAKE THIS VIDEO ADDED FUNCTION INSIDE THE CLIENTSOKETS.JS FILE!!!
-          // socket.emit('videoAdded', {body: $(this).attr('id') });
-          // return false;
-          addVideo($(this).attr('id'));
+				$(videoResult).on('click', function(){
+					// var id = $(document.createElement('li')).append($(this).attr('id'));
+					// $('#playlist').append(id);
+					
+					//MAKE THIS VIDEO ADDED FUNCTION INSIDE THE CLIENTSOKETS.JS FILE!!!
+					// socket.emit('videoAdded', {body: $(this).attr('id') });
+					// return false;
+					addVideo($(this).attr('id'));
 
-          //RELLY SHOULD CHECK FOR ERROR
+					//RELLY SHOULD CHECK FOR ERROR
 
-        });
+				});
 */
 
-         // console.log("response "+ i + ": " +response.items[i].snippet.thumbnails);
-    //});
+				 // console.log("response "+ i + ": " +response.items[i].snippet.thumbnails);
+		//});
 
-    // //really will want to do optimisitc rendering
-    // var postVideo = $.post('/videos', {data: response.result});
+		// //really will want to do optimisitc rendering
+		// var postVideo = $.post('/videos', {data: response.result});
 
-    // postVideo.done(function(data){
-    //   // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + data.data.items[0].id.videoId);
-    //   $.each(data.data.items, function(i){
-    //     // $('#search-container').append(
-    //     //   '<pre>' + JSON.stringify(data.data.items[i].id.videoId) + '</pre>'
-    //     // );
-    //   videoId.push(data.data.items[i].id.videoId);
-    //   });
+		// postVideo.done(function(data){
+		//   // console.log("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!" + data.data.items[0].id.videoId);
+		//   $.each(data.data.items, function(i){
+		//     // $('#search-container').append(
+		//     //   '<pre>' + JSON.stringify(data.data.items[i].id.videoId) + '</pre>'
+		//     // );
+		//   videoId.push(data.data.items[i].id.videoId);
+		//   });
 
-       
-    //     console.log("data successfuly stored")
-    // });
+			 
+		//     console.log("data successfuly stored")
+		// });
 
 
-  });
+	});
 }
 
 
@@ -236,12 +236,12 @@ var apiKey = 'AIzaSyDDCLIZDFCndntgpiPllCsDx98TKciDqfY';
 
 // Called automatically when JavaScript client library is loaded.
 function onClientLoad() {
-  gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+	gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
 }
 
 function onYouTubeApiLoad() {
-    // Step 2: Reference the API key
-    gapi.client.setApiKey(apiKey);
-    // window.setTimeout(checkAuth,1);
-    handleAPILoaded();
+		// Step 2: Reference the API key
+		gapi.client.setApiKey(apiKey);
+		// window.setTimeout(checkAuth,1);
+		handleAPILoaded();
 }
